a2afaa26cacf76ff07283c32f6fa153306f092a2
Fengdi Huang <96247250+JesseLovesGrace@users.noreply.github.com>
2024-03-10 13:53:27 +0200

    Update WebCrawlerZH.py

--- WebCrawlerZH.py
+++ WebCrawlerZH.py
@@ -3,99 +3,167 @@ import time
 import pandas as pd
 import os
 
-
-# Function to transform timestamp to human-readable format
-def trans_date(v_timestamp):
-    time_array = time.localtime(v_timestamp)
-    other_style_time = time.strftime("%Y-%m-%d %H:%M:%S", time_array)
-    return other_style_time
-
-
-# Function to transform gender tag to human-readable format
-def tran_gender(gender_tag):
-    if gender_tag == 1:
-        return 'male'
-    elif gender_tag == 0:
-        return 'female'
-    else:
-        return 'not defined'
-
-
-# Define the answer ID
-answer_id = 930193847
-
-# Define the URL for fetching comments
-url0 = f'https://www.zhihu.com/api/v4/comment_v5/answers/{answer_id}/root_comment?order_by=score&limit=20&offset='
-
-# Define the headers for the request
+# 请求头
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    'Referer': 'https://www.zhihu.com/question/291278869/answer/930193847',
-    'Cookie': '_zap=e0e06bd1-f135-4839-9c95-5bcd8c9b6447; d_c0=AHDZXZR97haPTtRWmtVprW1qJ2KInKq4Y-Y=|1686741951; __snaker__id=FFprrs1VNhxXIiKl; YD00517437729195%3AWM_NI=tDh4kTKhBTYakVx2jvb3gMfM7rU6XO9N9PtjjHOmSrUiyKd60OgwncxNjpnF3DNhjHMP8pZ3w1yzNXlzDpwnm51u02vsjsr2eOg9nYwTD9Wsato27a3mbio1fEFMkIc0V0g%3D; YD00517437729195%3AWM_NIKE=9ca17ae2e6ffcda170e2e6eebaf24781acabd0cb59a88a8eb6d15b839f9fb1c861a7899caed4449b958497bc2af0fea7c3b92ab89ca8d5ea6089b88eb5dc6ba98ab8d3e97ea2eda9aee547f5ae9bd7db45fb92f7b1f762aa98818ed27a97b7b7ace73ef491a998ed69bc92a691ed4ef19c8289e9448f90f891d939b5efb79bcd54b0a68c91f666afebff96b33ab6ada694f13aa791a9daed53b58c8cdad679a3aca1d3b66bf1b3ae8aee3ba69b8aaeea43a1f09ad1e637e2a3; YD00517437729195%3AWM_TID=ykwA%2B4kSWbpBEFBUUEPU0OGSYRDIee68; q_c1=dc63222d444c42c6b05ebe898a8e028e|1687003767000|1687003767000; q_c1=dc63222d444c42c6b05ebe898a8e028e|1693126237000|1687003767000; __utma=51854390.1113752316.1693126239.1693126239.1693126239.1; __utmv=51854390.100-1|2=registration_date=20230502=1^3=entry_date=20230502=1; z_c0=2|1:0|10:1708964656|4:z_c0|80:MS4xd2o5RVJnQUFBQUFtQUFBQVlBSlZUUjVydzJaSTBqZkE1bXdoc0dQNWdGbXFURlBzQjZwZ3FRPT0=|56bab5a8be19e433f519b184c200f4c487c2f9da25ed30641c65eafb7c712555; _xsrf=ba312381-a7d8-4032-98cf-53bdf894d200; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1709720633,1709740215,1709831653,1709906711; tst=r; SESSIONID=rUm1xuxLrtL8whP53VL9ByFUSrmRjrRVCpGJKJMd4pa; JOID=UlwRBE2Lya24eJHiGI3rfIVtMFgL_ZzDiAbgmlf_ueGDDv-2LfqlQ9p6leEa5-QmKyN466k0oD61aQLF7RaCJvY=; osd=W1EcC06CxKC3e5jvFYLodYhgP1sC8JHMiw_tl1j8sOyOAfy_IPeqQNN3mO4Z7ukrJCBx5qQ7oze4ZA3G5BuPKfU=; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1710008295; KLBRSID=e42bab774ac0012482937540873c03cf|1710008296|1710006844'
+    'x-zse-93': '101_3_3.0',
+    'x-ab-param': 'se_ffzx_jushen1=0;zr_expslotpaid=1;top_test_4_liguangyi=1;qap_question_author=0;tp_dingyue_video=0;tp_topic_style=0;tp_contents=2;qap_question_visitor= 0;pf_noti_entry_num=2;tp_zrec=1;pf_adjust=1;zr_slotpaidexp=1',
+    'x-ab-pb': 'CroB1wKmBDMFdAHgBAsE4wQZBRsAaQFWBVIL5ArHAjMEEQU0DLULdQSiAwoE0QT0C58C7AqbCz8AQAG5AtgCVwTBBNoE4AsSBU8DbAThBMoCNwVRBUMA9wNFBNcLzwsqBEIEoANWDNwL9gJsAzQEBwyEAjIDFAVSBbcD6QQpBWALfQI/BY4DZAS0CvgDFQUPC1ADVwPoA9YEagGMAnIDMgU3DMwCVQUBC0cAzAQOBbQAKgI7AqED8wP0A4kMEl0AAAAAAAABAAAAAAEAAAAAAAMAAAEFAAIBAAABFQABAQEAAQAAAgAAABUBAQALAAEAAQAAAAABAAACBAABAAABAAEBAAEAAQAAAAIBAAEAAQAAAQABAAAAAQAAAAA=',
+    'x-zst-81': '3_2.0ae3TnRUTEvOOUCNMTQnTSHUZo02p-HNMZBO8YD_ycXtucXYqK6P0E79y-LS9-hp1DufI-we8gGHPgJO1xuPZ0GxCTJHR7820XM20cLRGDJXfgGCBxupMuD_Io4cpr4w0mRPO7HoY70SfquPmz93mhDQyiqV9ebO1hwOYiiR0ELYuUrxmtDomqU7ynXtOnAoTh_PhRDSTFHOsaDH_8UYq0CN9UBFM6Hg1f_FOYrOGwBoYrgcCjBL9hvx1oCYK8CVYUBeTv6u1pgcMzwV8wwt1EbrL-UXBgvg0Z9N__vem_C3L8vCZfMS_Uhoftck1UGg0Bhw1rrXKZgcVQQeC-JLZ28eqWcOxLGo_KX3OsquLquoXxDpMUuF_ChUCCqkwe7396qOZ-Je8ADS9CqcmUuoYsq98yqLmUggYsBXfbLVL3qHMjwS_mXefOComiDSOkUOfQqX00UeBUcnXAh3mMD31bgOYSTSufuCYuDgCjqefWqHYeQSC',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+    'x-app-version': '6.42.0',
+    'sec-ch-ua-mobile': '?0',
+    'x-requested-with': 'fetch',
+    'x-zse-96': '2.0_aHtyee9qUCtYHUY81LF8NgU0NqNxgUF0MHYyoHe0NG2f',
+    'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    'accept': '*/*',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'cookie': r'KLBRSID=0a401b23e8a71b70de2f4b37f5b4e379|1694925229|1694925209; JOID=Ul4VA0jODPxTOiWCV8557xdFv-NAnEyJBFda_j2Oe5Ihe0XdZm6L-TY_JYRUrXdcCISuYhScbY8OuI9r4ztYUnA=; SESSIONID=TFuTnyzjCqhrBcY7NBTeCHVt1ZfgbAtWls8zn7U3VM0; osd=VlETC0_KA_pbPSGNUcZ-6xhDt-REk0qBA1NV-DWJf50nc0LZaWiD_jIwI4xTqXhaAIOqbRKUaosBvods5zReWnc=; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1694925201; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1694405587,1694655381,1694667763,1694925201; tst=r; _xsrf=b5e765de-8115-41fc-8668-2dcf5471434a; q_c1=394fe98a326046cd8cbeb01dc24ed8d3|1694925209000|1694925209000; z_c0=2|1:0|10:1694668373|4:z_c0|92:Mi4xSlFnWk1nQUFBQUFBa0pKUFpwdGVGeVlBQUFCZ0FsVk5JZUx2WlFBcU4xX05HYmV3THQ2eDl0X3R5UndVT19tOE53|99b66a086eb055bbde8dfb12b9681e01537019152d2385649fd42426c92c65b8; captcha_session_v2=2|1:0|10:1694667783|18:captcha_session_v2|88:eGZaMTdZOEJFV1M4ZTFxNnlWTFRhMnoyek5id3lxYm94eDlQVzB6eHpua2xoTVFpRGt4OHBsMXp2WDJvenc4RQ==|fbeecf11ae0fcf8a4b74fdd48b48319dcf7c8b89b05711cc357ce98cdc269656; _zap=4d8b5ec3-908d-4212-97e9-d974e1fd0f75; d_c0=AJCST2abXhePTuCA8dfyhGG2n9XagqWSo_4=|1694265961'
 }
 
-# Make the GET request
-r0 = requests.get(url0, headers=headers)
-total_comments = r0.json()['comment_counts']
-print(f'Total comments: {total_comments}')
-##########################################################################################
-# Still Getting: {'error': {'message': '请求参数异常，请升级客户端后重试。', 'code': 10003}}#
-##########################################################################################
 
-# Calculate the max page based on the total number of comments and limit per page
-limit_per_page = 20
-max_page = total_comments // limit_per_page + 1
-print('Max page:', max_page)
+def trans_date(v_timestamp):
+    """10位时间戳转换为时间字符串"""
+    timeArray = time.localtime(v_timestamp)
+    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    return otherStyleTime
 
-authors = []
-genders = []
-answer_urls = []
-author_homepages = []
-author_pics = []
-create_times = []
-contents = []
-child_tags = []
 
-# Loop over pages of comments
-for page in range(0, max_page):
-    offset = page * limit_per_page
-    url = url0 + str(offset)
-    r = requests.get(url, headers=headers)
-    comments = r.json()['data']
+def tran_gender(gender_tag):
+    """转换性别"""
+    if gender_tag == 1:
+        return '男'
+    elif gender_tag == 0:
+        return '女'
+    else:  # -1
+        return '未知'
 
-    for c in comments:  # Loop over comments in current page
-        # Process the comment
-        author = c['author']['member']['name']
-        authors.append(author)
-        gender_tag = c['author']['member']['gender']
-        genders.append(tran_gender(gender_tag))
-        # Add other attributes to respective lists
 
-        if c['child_comments']:  # Check if there are child comments
-            for child in c['child_comments']:  # Loop over child comments
-                # Process the child comment
-                child_author = child['author']['member']['name']
-                authors.append(child_author)
-                child_gender_tag = child['author']['member']['gender']
-                genders.append(tran_gender(child_gender_tag))
-                # Add other attributes to respective lists
+def comment_spider(v_result_file, v_answer_list):
+    for answer_id in v_answer_list:
+        url0 = 'https://www.zhihu.com/api/v4/answers/{}/root_comments?order=normal&limit=20&offset=0&status=open'.format(
+            answer_id)
+        r0 = requests.get(url0, headers=headers)  # 发送请求
+        total = r0.json()['common_counts']  # 一共多少条评论
+        print('一共{}条评论'.format(total))
+        # 判断一共多少页（每页20条评论）
+        max_page = int(total / 20)
+        print('max_page:', max_page)
+        # 开始循环爬取
+        for i in range(max_page):
+            offset = i * 20
+            url = 'https://www.zhihu.com/api/v4/answers/{}/root_comments?order=normal&limit=20&offset={}&status=open'.format(
+                answer_id,
+                str(offset))
+            r = requests.get(url, headers=headers)
+            print('正在爬取第{}页'.format(i + 1))
+            j_data = r.json()
+            comments = j_data['data']
+            # 如果没有评论了，就结束循环
+            if not comments:
+                print('无评论，退出循环')
+                break
+            # 否则开始爬取
+            # 定义空列表用于存数据
+            answer_urls = []  # 回答url
+            authors = []  # 评论作者
+            genders = []  # 作者性别
+            author_homepages = []  # 作者主页
+            author_pics = []  # 作者头像
+            create_times = []  # 评论时间
+            contents = []  # 评论内容
+            child_tag = []  # 评论级别
+            vote_counts = []  # 点赞数
+            ip_list = []  # IP属地
+            for c in comments:  # 一级评论
+                # 回答url
+                answer_urls.append('https://www.zhihu.com/answer/' + answer_id)
+                # 评论作者
+                author = c['author']['member']['name']
+                authors.append(author)
+                print('作者：', author)
+                # 作者性别
+                gender_tag = c['author']['member']['gender']
+                genders.append(tran_gender(gender_tag))
+                # 作者主页
+                homepage = 'https://www.zhihu.com/people/' + c['author']['member']['url_token']
+                author_homepages.append(homepage)
+                # 作者头像
+                pic = c['author']['member']['avatar_url']
+                author_pics.append(pic)
+                # 评论时间
+                create_time = trans_date(c['created_time'])
+                create_times.append(create_time)
+                # 评论内容
+                comment = c['content']
+                contents.append(comment)
+                print('评论内容：', comment)
+                # 评论级别
+                child_tag.append('一级评论')
+                # 点赞数
+                vote_counts.append(c['vote_count'])
+                # IP属地
+                ip_list.append(c['address_text'].replace('IP 属地', ''))
+                if c['child_comments']:  # 如果二级评论存在
+                    for child in c['child_comments']:  # 二级评论
+                        # 回答url
+                        answer_urls.append('https://www.zhihu.com/answer/' + answer_id)
+                        # 评论作者
+                        print('子评论作者：', child['author']['member']['name'])
+                        authors.append(child['author']['member']['name'])
+                        # 作者性别
+                        genders.append(tran_gender(child['author']['member']['gender']))
+                        # 作者主页
+                        author_homepages.append(
+                            'https://www.zhihu.com/people/' + child['author']['member']['url_token'])
+                        # 作者头像
+                        author_pics.append(child['author']['member']['avatar_url'])
+                        # 评论时间
+                        create_times.append(trans_date(child['created_time']))
+                        # 评论内容
+                        print('子评论内容：', child['content'])
+                        contents.append(child['content'])
+                        # 评论级别
+                        child_tag.append('二级评论')
+                        # 点赞数
+                        vote_counts.append(child['vote_count'])
+                        # IP属地
+                        ip_list.append(child['address_text'].replace('IP 属地', ''))
+            # 保存数据到csv
+            header = True
+            if os.path.exists(csv_file):  # 如果csv存在，不写表头，避免重复写入表头
+                header = False
+            df = pd.DataFrame(
+                {
+                    '回答url': answer_urls,
+                    '页码': [i + 1] * len(answer_urls),
+                    '评论作者': authors,
+                    '作者性别': genders,
+                    '评论内容': contents,
+                    '作者主页': author_homepages,
+                    '作者头像': author_pics,
+                    '评论时间': create_times,
+                    '评论级别': child_tag,
+                    '点赞数': vote_counts,
+                    'IP属地': ip_list,
+                }
+            )
+            # 保存到csv文件
+            df.to_csv(v_result_file, mode='a+', index=False, header=header, encoding='utf_8_sig')
 
-# Create DataFrame and save to CSV
-df = pd.DataFrame({
-    'Answer url': answer_urls,
-    'Page': [i + 1 for i in range(len(answer_urls))],
-    'Author': authors,
-    'Gender': genders,
-    'Author_homepages': author_homepages,
-    'Author_pics': author_pics,
-    'Create_time': create_times,
-    'Content': contents,
-    'Child_tag': child_tags,
-})
 
-# Define the file path for saving the CSV file
-desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-file_path = os.path.join(desktop_path, 'First_Test.csv')
-
-# Save to CSV file
-df.to_csv(file_path, mode='a+', index=False, header=True, encoding='utf_8_sig')
+if __name__ == '__main__':
+    csv_file = '知乎评论.csv'
+    # 如果csv存在，先删除，避免由于追加产生重复数据
+    if os.path.exists(csv_file):
+        print('文件存在，删除：{}'.format(csv_file))
+        os.remove(csv_file)
+    # 开始爬取, 相关的知乎热门回答id
+    comment_spider(v_result_file=csv_file,  # 保存文件名
+                   v_answer_list=['3422715693', '3421558669', '3424319850', '3421775502', '3422292503', '3423971470',
+                                  '3422577886', '3422583316', '3422489477', '3422876128', '3422885996', '3420991610',
+                                  '3422649294', '3422473052', '3421454261', '3422648915', '3422567030', '3423829261',
+                                  '3421575604', '3422652225', '3423480963', '3422781684', '3420976176', '3421290956'])
+    print('抓取完成 退出程序')
